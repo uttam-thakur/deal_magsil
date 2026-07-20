f0687590e99de1d7c177c9ca8c4f7d7af5be3e54
uttam <uttamthakur2016@gmail.com>
2026-07-21 01:14:16 +0530

    added gtag

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -134,3 +134,16 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
+// google tag
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-X08TH5NRLW"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-X08TH5NRLW');
+// </script>
